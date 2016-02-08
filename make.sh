5f51383fcd778b9da90bbfbf5a92eb550a5ee6eb
Kanit Srisuthep <kanit@sydney.usc.edu>
2016-02-08 01:00:42 -0800

    binary parsing and checking validity

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
-g++ -O3 -std=c++11 -fopenmp spc2.cpp -o spc2
-g++ -O3 -std=c++11 -fopenmp spc.cpp -o spc
-g++ -O3 -std=c++11 -fopenmp genZ.cpp -o genZ
+#g++ -O3 -std=c++11 -fopenmp spc2.cpp -o spc2
+g++ -O3 -std=c++11 -fopenmp hspc.cpp -o hspc
+#g++ -O3 -std=c++11 -fopenmp genZ.cpp -o genZ
